409570e12d9f3195ddc81ed1bc1060c81bee2ab1
maarcusvinicius <marcus.editor77@gmail.com>
2023-05-17 17:29:22 -0300

    complete

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fretefy'`, () => {
+  it(`should have as title 'fretefy-fullstack'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fretefy');
+    expect(app.title).toEqual('fretefy-fullstack');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fretefy app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('fretefy-fullstack app is running!');
   });
 });
